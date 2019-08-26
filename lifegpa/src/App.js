29b16f755a8330b2410fec765ae5d6e5a135d0fa
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-08-26 16:33:08 -0600

    NewGoalForm functional outside of the Frequency menu and styling

--- lifegpa/src/App.js
+++ lifegpa/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import FormikNewGoalForm from './components/NewGoalForm';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <FormikNewGoalForm/>
     </div>
   );
 }
