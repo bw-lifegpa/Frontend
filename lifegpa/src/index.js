476d397ac7d65386531e85548c8d63a5504037bd
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-08-26 13:27:44 -0600

    Routing updated, Redux frame added with blank initial state, default reducer, and Provider wrapper in index

--- lifegpa/src/index.js
+++ lifegpa/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { reducer } from './reducers';
+
+const store = createStore(reducer)
 
 ReactDOM.render(
+<Provider store={store}>
 <Router>
-<App />
-</Router>, document.getElementById('root'));
+    <App />
+</Router>
+</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
