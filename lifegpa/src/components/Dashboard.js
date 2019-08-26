cdafd4d8eada76f1d9296743d50e1d486640d37f
ch04937 <97hernandez.c@gmail.com>
2019-08-26 14:23:26 -0700

    merging login form to master

---
+++ lifegpa/src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return(
+        <div>
+            this is the Dashboard component
+        </div>
+    )
+}
+
+export default Dashboard;
--- lifegpa/src/components/PrivateRoute.js
+++ lifegpa/src/components/PrivateRoute.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router';
+import React from "react";
+import { Route, Redirect } from "react-router";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   return (
     <Route
       {...rest}
       render={props => {
-        if (localStorage.getItem('token')) {
+        if (localStorage.getItem("token")) {
           return <Component {...props} />;
         }
-        return <Redirect to="/login" />;
+        return <Redirect to="/" />;
       }}
     />
   );
