1730e5feb810ad779f3960a8a8f384bf3206e336
teddyhn <tngo97@gmail.com>
2019-08-26 18:20:13 -0700

    Axios call to mock data for demonstration purposes

--- lifegpa/src/components/Dashboard.js
+++ lifegpa/src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Dashboard.css'
 
 import Header from './Header';
@@ -6,13 +6,32 @@ import DashboardButtons from './DashboardButtons';
 import DashboardProfile from './DashboardProfile';
 import MiniDash from './MiniDash';
 import Footer from './Footer';
+import axios from 'axios';
 
 export default function Dashboard() {
+    const [userData, setUserData] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get('https://reqres.in/api/users/2')
+            .then(response => {
+                setUserData(response.data.data);
+            })
+            .catch(error => {
+                console.error('Server Error', error);
+            })
+    }, []);
+
+    console.log(userData);
+    
     return (
         <div className="dashboard-container">
             <Header />
             <div className="body">
-                <DashboardProfile />
+                <DashboardProfile 
+                    first_name={userData.first_name}
+                    last_name={userData.last_name}
+                />
                 <div className="dashboard-widgets">
                     <MiniDash />
                     <DashboardButtons />
--- lifegpa/src/components/DashboardProfile.js
+++ lifegpa/src/components/DashboardProfile.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-export default function DashboardProfile() {
-
+export default function DashboardProfile(props) {
     return (
         <div className="dashboard-profile-container">
             <div className="dashboard-profile">
                 <div className="profile-picture" />
-                <h1>Your Name</h1>
+                <h1>{`${props.first_name} ${props.last_name}`}</h1>
                 <h3>View Account</h3>
             </div>
         </div>
