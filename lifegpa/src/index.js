a44681407fcd0c1124d9bd242bfdce1f8926c3e6
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-08-27 16:36:11 -0600

    Create Goal form updated along with routing

--- lifegpa/src/index.js
+++ lifegpa/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { reducer } from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer)
+
+ReactDOM.render(
+<Provider store={store}>
+<Router>
+    <App />
+</Router>
+</Provider>, document.getElementById('root'));
