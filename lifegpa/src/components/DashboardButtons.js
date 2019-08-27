1f88f0699ed0e3ccc050cb705c12554e91fc1a7c
teddyhn <tngo97@gmail.com>
2019-08-27 13:58:15 -0700

    Added hi-fi buttons to Dashboard, more styling changes

--- lifegpa/src/components/DashboardButtons.js
+++ lifegpa/src/components/DashboardButtons.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import Track from '../designfiles/track-button.png';
+import Challenges from '../designfiles/challenges-button.png';
+import Advice from '../designfiles/advice-button.png';
+
 export default function DashboardButtons() {
     return (
         <div className="dashboard-nav-buttons">
             <NavLink to="/track">
                 <div className="dashboard-button-container">
-                    <div className="dashboard-button track">
-                    </div>
+                    <img className="dashboard-button track" src={Track} />
                     <h2>Track</h2>
                 </div>
             </NavLink>
             <NavLink to="/challenges">
                 <div className="dashboard-button-container">
-                    <div className="dashboard-button challenges">
-                    </div>
+                    <img className="dashboard-button challenges" src={Challenges} />
                     <h2>Challenges</h2>
                 </div>
             </NavLink>
             <NavLink to="/advice">
                 <div className="dashboard-button-container">
-                    <div className="dashboard-button advice">
-                    </div>
+                    <img className="dashboard-button challenges" src={Advice} />
                     <h2>Advice</h2>
                 </div>
             </NavLink>
