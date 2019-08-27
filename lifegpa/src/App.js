843bff2431cecd73c7cec5295f43f2d49136a180
teddyhn <tngo97@gmail.com>
2019-08-26 18:09:24 -0700

    Implemented react-burger-menu component as Nav

--- lifegpa/src/App.js
+++ lifegpa/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import FormikLogin from './components/Login';
 import Dashboard from './components/Dashboard';
+import Nav from './components/Nav';
 import PrivateRoute from './components/PrivateRoute';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Dashboard />
     </div>
   );
--- lifegpa/src/components/Header.js
+++ lifegpa/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css'
 
+
 export default function Header() {
     return (
         <div className="header">
