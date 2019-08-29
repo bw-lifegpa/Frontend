13ee84c6d59c747941eb17f22e39d4c98320c07a
LaVon Mejia <lavonmejia@gmail.com>
2019-08-29 14:34:10 -0400

    logout button functional

--- lifegpa/src/components/Header.js
+++ lifegpa/src/components/Header.js
@@ -3,14 +3,21 @@ import './Header.css'
 
 import Logo from '../designfiles/lifegpa-logo.png';
 import SignOut from '../designfiles/sign-out-icon.png';
+import {Link} from "react-router-dom";
 
 
 export default function Header() {
+    const signOutHandler = () => {
+        localStorage.clear();
+        window.location.href = '/'; 
+    };
+ 
+    
     return (
         <div className="header">
             <div className="header-icons">
                 <img className="logo" src={Logo} />
-                <img className="sign-out" src={SignOut} />
+                <img className="sign-out" src={SignOut} onClick= {signOutHandler} />
             </div>
         </div>
     )
