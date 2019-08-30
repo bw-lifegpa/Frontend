572b2fcc9b51472a67e8bfeb910b8657d7d06272
maxdavid <me@maxdavid.com>
2019-08-29 21:15:13 -0700

    Add 'mark complete' button. Autofill edit form
    * 'View Goals' page now has a button to mark the goal as complete
    * 'Edit Goal' form autopopulates with data of the existing goal

--- lifegpa/src/components/EditForm.js
+++ lifegpa/src/components/EditForm.js
@@ -1,19 +1,25 @@
-    
-import React, { useState } from "react";
-import { connect } from 'react-redux'
-import { editGoal, fetchGoal } from '../actions/index'
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { editGoal, fetchGoal } from '../actions/index';
 import Header from './Header';
 import Nav from './Nav';
 import Footer from './Footer';
 
-
-function EditForm (props) {
- const [goalState, setGoalState] = useState({
-    name: "",
-    description: "",
+function EditForm(props) {
+  const [goalState, setGoalState] = useState({
+    name: '',
+    description: ''
   });
 
-  const habit_id = parseInt(props.match.params.goal_id)
+  const habit_id = parseInt(props.match.params.goal_id);
+
+  useEffect(() => props.fetchGoal(habit_id), []);
+
+  useEffect(
+    () =>
+      setGoalState({ name: props.goal_name, description: props.description }),
+    [props.goal_name, props.description]
+  );
 
   const changeHandler = e => {
     e.preventDefault();
@@ -25,60 +31,56 @@ function EditForm (props) {
 
   const editGoal = e => {
     e.preventDefault();
-    props.editGoal(habit_id, goalState)
+    props.editGoal(habit_id, goalState);
     setGoalState({
       name: '',
       description: ''
-    })
-    props.history.push(`/goals/${props.match.params.id}`)
-  }
+    });
+    props.history.push(`/goals/${props.match.params.id}`);
+  };
 
-  if (goalState){
-  return (
-        <div>
-            <Header/>
-            <Nav user_id={props.match.params.id}/>
-            <h1>Edit:</h1>
-              <form onSubmit={editGoal}>
-                <input 
-                      type="text"
-                      placeholder="goal name"
-                      name="name"
-                      value={goalState.name}
-                      onChange={changeHandler}
-                      />
-                  <input  
-                      type="text"
-                      placeholder="enter description"
-                      name="description"
-                      value={goalState.description}
-                      onChange={changeHandler}
-                      />   
-                    <button type="submit">
-                      Update Goal
-                    </button>
-                </form>
-            <Footer/>
-        </div>
-    )
+  if (goalState) {
+    return (
+      <div>
+        <Header />
+        <Nav user_id={props.match.params.id} />
+        <h1>Edit:</h1>
+        <form onSubmit={editGoal}>
+          <input
+            type='text'
+            placeholder='goal name'
+            name='name'
+            value={goalState.name}
+            onChange={changeHandler}
+          />
+          <input
+            type='text'
+            placeholder='enter description'
+            name='description'
+            value={goalState.description}
+            onChange={changeHandler}
+          />
+          <button type='submit'>Update Goal</button>
+        </form>
+        <Footer />
+      </div>
+    );
   }
-  return (
-      <div />
-  )
+  return <div />;
 }
 
 const mapStateToProps = state => {
-  console.log(state)
+  console.log(state);
   return {
     error: state.error,
     isFetching: state.isFetching,
     goal_name: state.goal_name,
     description: state.description,
     habit_id: state.habit_id
-  }
-}
+  };
+};
 
 export default connect(
-    mapStateToProps,
-    { fetchGoal, editGoal }
-)(EditForm)
+  mapStateToProps,
+  { fetchGoal, editGoal }
+)(EditForm);
--- lifegpa/src/components/GoalCard.js
+++ lifegpa/src/components/GoalCard.js
@@ -1,61 +1,97 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { fetchUser, updateHabitId } from '../actions';
+import { fetchUser, updateHabitId, fetchUserCompletedHabits } from '../actions';
 import { axiosWithAuth } from '../utilities/axiosWithAuth';
 import moment from 'moment';
 
-const GoalCard = (props) => {
+const GoalCard = props => {
+  useEffect(() => {
+    props.fetchUser(props.match.params.id);
+    props.updateHabitId(props.goal.habit_id);
+    props.fetchUserCompletedHabits(props.match.params.id);
+  }, []);
 
-    useEffect(() => {
-        props.fetchUser(props.match.params.id);
-        props.updateHabitId(props.goal.habit_id)
-    }, []);
+  const deleteHandler = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .delete(`https://lifegpa-api.herokuapp.com/habits/${props.goal.habit_id}`)
+      .then(res => {
+        window.location.reload();
+        console.log(res.data);
+      })
 
-    const deleteHandler = (e) => {
-        e.preventDefault();
-        axiosWithAuth()
-        .delete(`https://lifegpa-api.herokuapp.com/habits/${props.goal.habit_id}`)
-        .then(res=> {
-            window.location.reload();
-            console.log (res.data)})
+      .catch(res => console.log(res.err));
+  };
 
-        .catch(res => console.log (res.err))
+  const completeHandler = e => {
+    const { id } = props.match.params;
+    const { habit_id } = props.goal;
+    e.preventDefault();
+    axiosWithAuth()
+      .post(`https://lifegpa-api.herokuapp.com/users/${id}/habits/completed`, {
+        habit_id: habit_id
+      })
+      .then(res => {
+        console.log(res.data);
+        props.fetchUserCompletedHabits(props.match.params.id);
+      });
+  };
 
-    }
-    return (
-        <div className='goal-card'>
-            
-            <div className='goal-container'>
-                <h1>{props.goal.name}</h1>
-                <h3>{props.goal.description}</h3>
-                
-                <h6>Created At: {moment(props.goal.start_date).format("MMM Do YYYY")}</h6>
+  return (
+    <div className='goal-container'>
+      <div>
+        <h1>{props.goal.name}</h1>
+        <h3>{props.goal.description}</h3>
+      </div>
 
-                <div className='button-container'>
-                    <Link to={`/edit/${props.id}/${props.goal.habit_id}`}>
-                        <button className='edit'>Edit</button>
-                    </Link>
-                    <button className='edit' onClick = {deleteHandler} >Delete</button>
-                    <button className='edit' >Goal Completed</button>
-                </div>
-            </div>
+      <div>
+        <div className='goal-details'>
+          <h4>
+            Goal completed{' '}
+            {props.completed.length
+              ? props.completed.filter(
+                  habit => habit.habit_id === props.goal.habit_id
+                ).length
+              : '0'}{' '}
+            times
+          </h4>
+          <h5>
+            started on {moment(props.goal.start_date).format('MMMM Do, YYYY')}
+          </h5>
         </div>
-    )
 
-}
+        <div className='button-container'>
+          <button className='mark-complete' onClick={completeHandler}>
+            Mark Completed
+          </button>
+          <div className='edit-buttons'>
+            <Link to={`/edit/${props.id}/${props.goal.habit_id}`}>
+              <button>Edit</button>
+            </Link>
+            <button onClick={deleteHandler}>Delete</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 const mapStateToProps = state => {
-    return {
-        error: state.error,
-        isFetching: state.isFetching,
-        id: state.id,
-        username: state.username,
-        first_name: state.first_name,
-        last_name: state.last_name,
-        email: state.email,
-        habit_id: state.habit_id,
-        created_at:state.created_at
-    }
-}
+  return {
+    error: state.error,
+    isFetching: state.isFetching,
+    id: state.id,
+    username: state.username,
+    first_name: state.first_name,
+    last_name: state.last_name,
+    email: state.email,
+    habit_id: state.habit_id,
+    created_at: state.created_at,
+    completed: state.completed
+  };
+};
 
-export default connect(mapStateToProps, { fetchUser, updateHabitId })(GoalCard);
+export default connect(
+  mapStateToProps,
+  { fetchUser, updateHabitId, fetchUserCompletedHabits }
+)(GoalCard);
